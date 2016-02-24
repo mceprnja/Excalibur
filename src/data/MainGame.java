e9fd1bd43a2d8b48eb5144c29a374f315a20768c
JewishTraitor <franko.macetic@net.hr>
2016-02-24 12:12:37 +0100

    added figure rendering and initial figure setup

--- src/data/MainGame.java
+++ src/data/MainGame.java
@@ -3,54 +3,144 @@ package data;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.lwjgl.input.Keyboard;
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+
+import utilities.Consts;
+import utilities.FigureType;
+import utilities.MapTile;
+import utilities.TileType;
 
 public class MainGame extends BasicGame {
 
-    public static int TILE_HEIGHT = 64;
-    public static int TILE_WIDTH = 64;
-    public static int SCREEN_WIDTH = TILE_HEIGHT * 10;
-    public static int SCREEN_HEIGHT = TILE_WIDTH * 10;
-    private MapTile mapTile = new MapTile(SCREEN_WIDTH, SCREEN_HEIGHT);
-   
+    private MapTile mapTile = new MapTile(Consts.SCREEN_WIDTH, Consts.SCREEN_HEIGHT);
+
+    private Input input;
+    private int[] capacities = { 6, 1, 1, 1, 8, 2, 4, 4, 1, 3, 4, 5 };
+    private int[] remaining = { 6, 1, 1, 1, 8, 2, 4, 4, 1, 3, 4, 5 };
+    private int index = 0;
+    private FigureType current;
+    private Image image;
+    private int oldTileX, oldTileY;
+    int mouseX, mouseY;
+    boolean wasDown = false;
 
     public static void main(String[] args) {
         try {
             AppGameContainer appgc;
             appgc = new AppGameContainer(new MainGame("Excalibur"));
-            appgc.setDisplayMode(SCREEN_WIDTH, SCREEN_HEIGHT, false);
+            appgc.setDisplayMode(Consts.SCREEN_WIDTH, Consts.SCREEN_HEIGHT, false);
             appgc.setTargetFrameRate(100);
             appgc.setIcon("images/icon.png");
-//            appgc.setAlwaysRender(true);
-//            appgc.setClearEachFrame(false);
+            // appgc.setAlwaysRender(true);
+            // appgc.setClearEachFrame(false);
             appgc.start();
         } catch (SlickException ex) {
             Logger.getLogger(MainGame.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
+
     public MainGame(String gamename) {
         super(gamename);
     }
 
     @Override
     public void init(GameContainer container) throws SlickException {
-        // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void update(GameContainer container, int delta) throws SlickException {
-        // TODO Auto-generated method stub
-        
+        input = container.getInput();
+
+        if (input.isKeyPressed(Input.KEY_1)) {
+            if (remaining[index] > 0) {
+                int mouseX = input.getMouseX();
+                int mouseY = input.getMouseY();
+                if (mapTile.canPlaceAt(mouseX, mouseY)) {
+                    mapTile.setFigureAt(mouseX, mouseY, index);
+                    remaining[index]--;
+                    showRemaining();
+                }
+            }
+        } else if (input.isKeyPressed(Input.KEY_2)) {
+            if (remaining[index] < capacities[index]) {
+                mouseX = input.getMouseX();
+                mouseY = input.getMouseY();
+                if (mapTile.getFigureAt(mouseX, mouseY) != FigureType.Null) {
+                    FigureType current = mapTile.getFigureAt(mouseX, mouseY);
+                    mapTile.clearTile(mouseX, mouseY);
+                    remaining[current.ordinal()]++;
+                    showRemaining();
+                }
+            }
+        }
+
+        if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+            mouseX = input.getMouseX();
+            mouseY = input.getMouseY();
+        }
+        if (input.isMousePressed(Input.MOUSE_LEFT_BUTTON)) {
+            current = mapTile.getFigureAt(mouseX, mouseY);
+            oldTileX = mouseX;
+            oldTileY = mouseY;
+            mapTile.clearTile(oldTileX, oldTileY);
+            wasDown = true;
+        }
+        if (!input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) && wasDown) {
+            if (current == FigureType.Null) {
+                System.out.println("it was null");
+            } else {
+                if (mapTile.canPlaceAt(mouseX, mouseY)) {
+                    mapTile.setFigureAt(mouseX, mouseY, current.ordinal());
+                } else {
+                    mapTile.setFigureAt(oldTileX, oldTileY, current.ordinal());
+                }
+            }
+            wasDown = false;
+            current = null;
+        }
+
+        if (input.isKeyPressed(Keyboard.KEY_TAB)) {
+            index++;
+            index %= remaining.length;
+
+            System.out.println("Now placing " + FigureType.values()[index].getName());
+        }
+
+        if (input.isKeyPressed(Keyboard.KEY_SPACE)) {
+            showRemaining();
+        }
+        if (input.isKeyPressed(Keyboard.KEY_R)) {
+            mapTile.clearAll();
+            for (int i = 0; i < remaining.length; i++) {
+                remaining[i] = capacities[i];
+            }
+            showRemaining();
+        }
     }
+
     @Override
     public void render(GameContainer container, Graphics g) throws SlickException {
-        // TODO Auto-generated method stub
         mapTile.render(g);
+
+        if (current != null) {
+            image = new Image(current.getPath());
+            g.drawImage(image, mouseX, mouseY, mouseX + Consts.TILE_WIDTH, mouseY + Consts.TILE_HEIGHT, 0, 0,
+                    image.getWidth(), image.getHeight());
+        }
+    }
+
+    public void showRemaining() {
+        for (int i = 0; i < remaining.length; i++) {
+            System.out.print(remaining[i] + " " + FigureType.values()[i].getName() + ", ");
+        }
+        System.out.println();
     }
 }
---
+++ src/utilities/MapTile.java
@@ -0,0 +1,135 @@
+package utilities;
+
+import org.newdawn.slick.Color;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+public class MapTile {
+    private int[][][] matrix;
+    private int xMax, yMax;
+    private Image image;
+
+    public MapTile(final int WIDTH, final int HEIGHT) {
+        xMax = WIDTH / Consts.TILE_WIDTH;
+        yMax = HEIGHT / Consts.TILE_HEIGHT;
+        matrix = new int[xMax][yMax][2];
+        for (int i = 0; i < xMax; i++) {
+            for (int j = 0; j < yMax; j++) {
+                if (j == 4 && i == 2)
+                    matrix[i][j][0] = TileType.Water.ordinal();
+                else if ((j == 5 || j == 4) && (i == 2 || i == 3))
+                    matrix[i][j][0] = TileType.Water.ordinal();
+                else if ((j == 5 || j == 4) && (i == 6 || i == 7))
+                    matrix[i][j][0] = TileType.Water.ordinal();
+                else
+                    matrix[i][j][0] = TileType.Dirt.ordinal();
+                matrix[i][j][1] = -1;
+            }
+        }
+    }
+
+    private Image getImage(int tileIndex) throws SlickException {
+        Image image;
+        String path = "images/" + TileType.values()[tileIndex] + ".png";
+        image = new Image(path);
+
+        return image;
+    }
+
+    public void render(Graphics g) throws SlickException {
+        for (int i = 0; i < xMax; i++) {
+            for (int j = 0; j < yMax; j++) {
+                int xPos = Consts.TILE_WIDTH * i + i;
+                int yPos = Consts.TILE_HEIGHT * j + j;
+                int x2 = xPos + Consts.TILE_WIDTH;
+                int y2 = yPos + Consts.TILE_HEIGHT;
+                g.drawImage(getImage(matrix[i][j][0]), xPos, yPos, x2, y2, 0, 0, Consts.TILE_WIDTH, Consts.TILE_HEIGHT);
+                
+                if (matrix[i][j][1] != -1) {
+                    image = new Image(FigureType.values()[matrix[i][j][1]].getPath());
+                    
+                    g.drawImage(image, xPos, yPos, xPos + Consts.TILE_WIDTH, yPos + Consts.TILE_HEIGHT, 0, 0,
+                            image.getWidth(), image.getHeight());
+                }
+            }
+        }
+
+    }
+
+    public int getTileX(int x) {
+        int xTile = (int) Math.floor(x / (Consts.TILE_WIDTH + 1)); // padding
+        if (xTile >= xMax)
+            xTile = 0;
+        return xTile;
+    }
+
+    public int getTileY(int y) {
+        int yTile =  (int) Math.floor(y / (Consts.TILE_HEIGHT + 1)); // padding
+        if (yTile >= xMax)
+            yTile = 0;
+        return yTile;
+    }
+
+    public TileType getTileType(int x, int y) {
+        return TileType.values()[matrix[getTileX(x)][getTileY(y)][0]];
+    }
+
+    public void setFigureAt(int x, int y, int figure) {
+        int xTile =  (int) Math.floor(x / (Consts.TILE_WIDTH + 1)); // padding
+        int yTile =  (int) Math.floor(y / (Consts.TILE_HEIGHT + 1)); // padding
+        if (xTile >= xMax || yTile >= yMax) {
+            return;
+        }
+
+        matrix[xTile][yTile][1] = figure;
+    }
+
+    public void clearTile(int x, int y) {
+        int xTile =  (int) Math.floor(x / (Consts.TILE_WIDTH + 1)); // padding
+        int yTile =  (int) Math.floor(y / (Consts.TILE_HEIGHT + 1)); // padding
+        if (xTile >= xMax || yTile >= yMax) {
+            return;
+        }
+        matrix[xTile][yTile][1] = -1;
+    }
+
+    public void clearAll() {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j][1] = -1;
+            }
+        }
+    }
+    
+    public boolean canPlaceAt(int x, int y) {
+        int xTile =  (int) Math.floor(x / (Consts.TILE_WIDTH + 1)); // padding
+        int yTile =  (int) Math.floor(y / (Consts.TILE_HEIGHT + 1)); // padding
+        
+        if (xTile >= xMax || yTile >= yMax) {
+            return false ;
+        }
+        
+        return (matrix[xTile][yTile][0] == 0 && matrix[xTile][yTile][1] == -1);
+    }
+
+    public FigureType getFigureAt(int x, int y) {
+        FigureType type = null;
+        if (matrix[getTileX(x)][getTileY(y)][1] < 0) {
+            type = FigureType.Null;
+        } else {
+            type = FigureType.values()[matrix[getTileX(x)][getTileY(y)][1]];
+        }
+
+        return type;
+    }
+    
+    public void drawFigure(int x, int y, FigureType figure) {
+        
+    }
+
+    public void update() {
+
+    }
+
+}
